2f70f696dbfa7ce113ec36ad00a0fd92353ddd5a
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-04-08 00:49:54 -0300

    7- add webpack dev server to run application

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        port: 3000,
+        static: path.resolve(__dirname, 'public'),
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html'),
