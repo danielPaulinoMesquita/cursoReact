e6d7814ecbc5d5356d9c26a06570b120dfc942d7
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-04-07 23:04:53 -0300

    4.1- configuring structure from react.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import {render} from "react-dom";
-import React from "react";
 import {App} from "./App";
 
-render(<h1>Testando</h1>, document.getElementById('root'))
+render(<App/>, document.getElementById('root'))
