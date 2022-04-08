054e356866f0d6af074a17d959c73e303f697eb4
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-04-07 23:20:12 -0300

    5- Solved the problem that warning

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
 const path = require('path');
-// require pertence ao NODE
-
-// module.exports = {
-//     entry: 'src/index.jsx', <-- dependendo do sistema operacional ele pode falhar (windows usa \\) entry indica o prinncipal arquivo, o arquivo inicial
-// };
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
