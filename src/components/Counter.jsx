22192822672225a9ac2f75f8bfa5da5a522e8006
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-04-24 03:06:57 -0300

    13- added style for RepositoryList and added request for api git

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,7 +3,7 @@ import {useState} from "react";
 // useState é um hook
 export function Counter() {
     // useState trabalha com arrays, ele traz dois resultados no array
-    // por conveção da comunidade, o segundo elemento do array é para modificar
+    // por convenção da comunidade, o segundo elemento do array é para modificar
     let [counter, setCounter] = useState(0);
     function increment(){
         setCounter(counter + 1);
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,25 @@
 import {RepositoryItem} from "./RepositoryItem";
-
-const repository =  {
-    repository: "Testando",
-    description:"Forms in React",
-    link:"https://www.google.com.br",
-};
+import '../styles/repository.scss';
+import {useEffect, useState} from "react";
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then( res => res.json())
+            .then( data => {
+                setRepositories(data);
+                console.log(data);
+            });
+    }, []);
+
     return(
         <section className="repository-list">
-            <h2>Lista de repositórios </h2>
-
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem/>
-                <RepositoryItem />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}
             </ul>
         </section>
     )
