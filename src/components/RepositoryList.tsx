eae283026ea2c45902476d5f6ebcb189b0e73779
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-04-24 18:32:28 -0300

    14- converting project to typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,13 @@ import {RepositoryItem} from "./RepositoryItem";
 import '../styles/repository.scss';
 import {useEffect, useState} from "react";
 
+type RepositoryType = {
+    name:string,
+    description: string,
+    html_url: string
+}
 export function RepositoryList(){
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<RepositoryType[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -17,8 +22,8 @@ export function RepositoryList(){
     return(
         <section className="repository-list">
             <ul>
-                {repositories.map(repository => {
-                    return <RepositoryItem key={repository.name} repository={repository}/>
+                {repositories.map(rep => {
+                    return <RepositoryItem key={rep.name} repository={rep}/>
                 })}
             </ul>
         </section>
